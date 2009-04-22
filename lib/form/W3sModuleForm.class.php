505908fd7b316a6cd7687dbaed4bf0f9c7e45f1b
Yevgeniy A. Viktorov <wik@osmonitoring.com>
2009-04-22 19:16:10 +0300

    add w3s_module form

---
+++ lib/form/W3sModuleForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * W3sModule form.
+ *
+ * @package    sfW3studioCms
+ * @subpackage form
+ * @author     Yevgeniy Viktorov <wik@osmonitoring.com>
+ * @version    SVN: $Id$
+ */
+class W3sModuleForm extends BaseW3sModuleForm
+{
+  public function configure()
+  {
+  }
+}
