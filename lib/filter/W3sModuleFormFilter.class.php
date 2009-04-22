f6c9910ecb27b187459afe2f829dc2d237024b9e
Yevgeniy A. Viktorov <wik@osmonitoring.com>
2009-04-22 19:15:45 +0300

    add w3s_module form filter

---
+++ lib/filter/W3sModuleFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * W3sModule filter form.
+ *
+ * @package    sfW3studioCms
+ * @subpackage filter
+ * @author     Yevgeniy Viktorov <wik@osmonitoring.com>
+ * @version    SVN: $Id$
+ */
+class W3sModuleFormFilter extends BaseW3sModuleFormFilter
+{
+  public function configure()
+  {
+  }
+}
